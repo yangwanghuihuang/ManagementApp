38afa7abc62da369afeed8265571fa344f333e27
zhanglr3 <zhanglr3@asiainfo.com>
2020-01-21 15:42:47 +0800

    update

--- src/pages/workflow/Workflow.vue
+++ src/pages/workflow/Workflow.vue
@@ -10,8 +10,7 @@
           </van-swipe-item>
           </van-swipe>
          </div>
-         <div class="SiderBar">
-      
+         <div class="SiderBar">     
          <van-sidebar v-model="activeKey"  @change="onChange">
             <van-sidebar-item title="故障上报" />
             <van-sidebar-item title="业主公告"  />
@@ -35,8 +34,8 @@ export default {
          '/static/images/app_index1.png',
         '/static/images/app_index2.png',
 
-        // 'https://img.yzcdn.cn/vant/apple-1.jpg',
-        // 'https://img.yzcdn.cn/vant/apple-2.jpg'
+        '/static/images/apple-1.png',
+       '/static/images/apple-2.jpg',
       ],
       // option: {
       //       title: {
@@ -102,105 +101,18 @@ export default {
 <style lang="less" scoped>
   .www {
     height: 100%;
-    .workflowBg {
-      height: 100%;
-      background-image: url(../../../static/images/bg_workflow.png);
+    .swipPic {
+      height: 5%;
+      width: 100%;
+      // background-image: url(../../../static/images/bg_workflow.png);
       background-repeat : no-repeat;
       background-size: 100% 100%;
-     padding-bottom: 89%;
-      // padding-bottom: 20rem;
-      // .echar{
-      //   width: 100%;
-      //   height: 0%;
-      // }
-      .workflow-top {
-        padding-top: 0.8rem;
-        padding-left: 1.8rem;
-      }
-
-      .workflow-top-text .top {
-        font-size: 2rem;
-        color: #ffffff;
-        margin-bottom: 1rem;
-        height: 30%;
-      }
-
-      .workflow-top-text div {
-        color: #ffffff;
-        font-size: 0.8rem;
-        margin-bottom: 0.5rem;
-      }
-
-      .button {
-        display: block;
-        text-align: center;
-        line-height: 1.8rem;
-        background-color: #fff;
-        color: blue;
-        margin-left: 1.3rem !important;
-        height: 1.8rem;
-        border-radius: 10%;
-        border: none;
-        cursor: pointer;
-        border-top-right-radius: 0px;
-        font-size: 0.8rem !important;
-      }
-
-      .workflow-header {
-        text-align: center;
-        height: 5%;
-        font-size: 1.5rem;
-       margin-top: 27%;
-       font-weight: 700;
-      }
-      .headerText{
-           font-weight: 700;
-      }
-      .workflow-content {
-        height: 90%;
-        display: flex;
-        flex-direction: column;
-        .workflow-content-step1, .workflow-content-step2, .workflow-content-step3 {
-          width: 85%;
-          height: 30%;
-          text-align: left;
-          background-color: #fff;
-          margin: 1rem auto;
-        }
-      }
-
-      .title1 {
-        display: flex;
-        flex-direction: row;
-        margin-top: 1rem;
-      }
-
-      .text01 {
-        width: 7%;
-        font-size: 45px;
-        color: #666;
-        margin-left: 50px;
-      }
-
-      .title {
-        width: 48%;
-        margin-left: 20px;
-        line-height: 45px;
-        font-weight: 700;
-      }
-
-      p {
-        margin-left: 20px;
-        font-size: 12px !important;
-        line-height: 20px;
-        margin-top: 0.3rem;
-        color: #7d7878;
-      }
-
-      .workflow-content-step3 {
-        margin-bottom: 6.9rem;
-      }
+    margin-bottom: 25rem;
     }
+    .SiderBar{
+      height: 60%;
+    }
+     
   }
 
 </style>
